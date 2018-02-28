345ea6ccc0ce2615bf19f500f8b688f1bef04b2b
RJ <jensenrs@gmail.com>
2018-02-27 16:10:45 -0800

    basic layout and function set up

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+
+  state = {
+    ships: {
+      shipOne: [[1, 'A'], [1, 'B'], [1, 'C']],
+      shipTwo: [[2, 'B'], [2, 'C']]
+    }
+
+  }
+
+
+  guess = (e) => {
+    const column = parseInt(e.target.id[0], 10)
+    const row = e.target.id[1]
+
+    const ships = this.state.ships
+    let hit = false
+    for(var i in ships){
+
+      for(let k = 0; k < ships[i].length; k++){
+
+        if(ships[i][k][0] === column && ships[i][k][1] === row){
+          console.log("HIT!!!");
+          hit = true
+
+        }
+
+      }
+
+    }
+
+    if (hit){
+      e.target.classList.add('hitShip')
+      e.target.classList.remove('target')
+    } else {
+      e.target.classList.add('missShip')
+      e.target.classList.remove('target')
+    }
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="grid-wrapper">
+
+          <div className="square-wrapper">
+            <div className="square letter"></div>
+            <div className="square">1</div>
+            <div className="square">2</div>
+            <div className="square">3</div>
+            <div className="square">4</div>
+            <div className="square">5</div>
+            <div className="square">6</div>
+            <div className="square">7</div>
+            <div className="square">8</div>
+            <div className="square">9</div>
+            <div className="square">10</div>
+          </div>
+          <div className="square-wrapper">
+            <div className="square letter">A</div>
+            <div onClick={ this.guess } className="square target" id="1A"></div>
+            <div onClick={ this.guess } className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+          </div>
+          <div className="square-wrapper">
+            <div className="square letter">B</div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+          </div>
+          <div className="square-wrapper">
+            <div className="square letter">C</div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+            <div className="square target"></div>
+          </div>
+
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
